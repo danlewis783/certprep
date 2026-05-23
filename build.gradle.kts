da14ddfcb410ee3643de6182c2e148540deb4123
Dan Lewis <20071+danlewis783@users.noreply.github.com>
2026-05-23 11:10:36 -0500

    i like Google Guava, but it's not used by this app

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,10 @@ val appName = "CertPrep"
 val appMainClass = "acme.certprep.CertPrep"
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    // This dependency is used by the application.
-    implementation(libs.guava)
     implementation(libs.jspecify)
     implementation(libs.slf4j.api)
     runtimeOnly(libs.logback.classic)
@@ -27,14 +24,12 @@ val toolsSourceSet = sourceSets.create("tools") {
 
 testing {
     suites {
-        // Configure the built-in test suite
         named<JvmTestSuite>("test") {
             useJUnitJupiter(libs.versions.junit.jupiter.get())
         }
     }
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(11))
